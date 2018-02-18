c856a68e55b829abd92db5d132b4a5f9305723e8
kevlondos <kevneverpays@yahoo.com>
2018-02-18 14:39:01 -0600

    Test commit from intelliJ

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
+
+
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World@@@@@@@QQ!");
+		System.out.println("Trying to update Github from intellij");
 	}
 }
